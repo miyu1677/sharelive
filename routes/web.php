c5f297f6201a29842d08abe6f5d026aeb0798515
miyu1677 <miyu0707k@outlook.jp>
2025-04-11 13:59:06 +0000

    Base implementation of posting function

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SharePostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/posts', [SharePostController::class, 'index']);
